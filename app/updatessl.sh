200ad999924d55e262027734a52ab4e8d15ffdc4
Alberto Roura <albertoroura@gmail.com>
2019-02-02 17:42:24 +1000

    Updating ssl generating

--- app/updatessl.sh
+++ app/updatessl.sh
@@ -2,28 +2,22 @@
 
 _SCRIPT_="$0"
 
-ACME_BIN="/acme.sh/acme.sh --home /acme.sh --config-home /acmecerts"
-
-DEFAULT_CONF="/etc/nginx/conf.d/default.conf"
-
-CERTS="/etc/nginx/certs"
-
 updatessl() {
   nginx -t && nginx -s reload
-  if grep ACME_DOMAINS $DEFAULT_CONF ; then
-    for d_list in $(grep ACME_DOMAINS $DEFAULT_CONF | cut -d ' ' -f 2);
+  if grep ACME_DOMAINS /etc/nginx/conf.d/default.conf ; then
+    for d_list in $(grep ACME_DOMAINS /etc/nginx/conf.d/default.conf | cut -d ' ' -f 2);
     do
       d=$(echo "$d_list" | cut -d , -f 1)
-      $ACME_BIN --issue \
+      /acme.sh/acme.sh --home /acme.sh --config-home /acmecerts --issue \
       -d $d_list \
       --nginx \
-      --fullchain-file "$CERTS/$d.crt" \
-      --key-file "$CERTS/$d.key" \
+      --fullchain-file "/etc/nginx/certs/$d.crt" \
+      --key-file "/etc/nginx/certs/$d.key" \
       --reloadcmd "nginx -t && nginx -s reload"
     done
 
-     #generate tengine conf again.
-    docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf
+     # Generate Tengine `default.conf` again.
+    docker-gen /app/default.conf /etc/nginx/conf.d/default.conf
   else
     echo "skip updatessl"
   fi
